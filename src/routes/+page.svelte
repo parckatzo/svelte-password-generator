1e11f87c5d6364cc32ca06616c90aedc418b0a81
parckatzo <sabadogigante1717@gmail.com>
2022-12-08 12:16:53 -0300

    finseshed

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,5 +5,21 @@ import Password from "./Password.svelte";
   <Password />
 </div>
 
-<style src="./global.css">
+<style>
+  :global(*){
+    margin: 0;
+    padding: 0;
+  }
+  div{
+    background-size: 100% 100%;
+    background-position: 0px 0px;
+    background-image: radial-gradient(100% 60% at -4% -3%, #ac4018 0%, #0e0e0e 100%);
+    min-height: 100vh;
+    min-width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+  }
+  
 </style>
--- src/routes/Password.svelte
+++ src/routes/Password.svelte
@@ -1,12 +1,14 @@
-<main>
+  <main>
     <div>
       <h1>Password Generator</h1>
       <h2>{password}</h2>
       <button on:click={makeRandomPw} id="code">Generate password</button>
       <div class="input-group">
+        <!-- svelte-ignore a11y-label-has-associated-control -->
         <label>Password length (4-20)</label>
-        <input type="range" id="length" min='4' max='20' step="1" bind:value={passwordLength} />
+        <input type="range" id="length" class="slider" min='4' max='20' step="1" bind:value={passwordLength} />
         <span>{passwordLength}</span>
+        <div class="background"></div>
       </div>
     </div>
   </main>
@@ -28,9 +30,73 @@
   </script>
   
   <style>
-      *{
-          padding: 0;
-          margin: 0;
-          box-sizing: border-box;
-      }
+        *{
+            padding: 0;
+            margin: 0;
+        }
+        main{
+          color: rgba(255, 255, 255, 0.801);
+        }
+        div{
+          display: flex;
+          flex-direction: column;
+          justify-content: center;
+          align-items: center;
+        }
+        h1{
+          margin: 10px;
+          font-size: 60px;
+        }
+        h2{
+          text-align: center;
+          margin: 10px;
+          padding: 6px;
+          background-color: rgb(27, 27, 27);
+          transition: all 300ms;
+          min-width: 399px;
+        }
+        .input-group{
+          display: flex;
+          flex-direction: row;
+          margin: 20px;
+          font-size: 20px;
+        }
+        button{
+        background-color: #f76027;
+        color: rgb(0, 0, 0);
+        border: none;
+        border-radius: 4px;
+        padding: 0.5rem;
+        text-transform: uppercase;
+        min-width: 200px;
+        }
+        .slider {
+    -webkit-appearance: none;
+    width: 100%;
+    height: 25px;
+    background: #f75f2763;
+    outline: none;
+    opacity: 0.7;
+    -webkit-transition: .2s;
+    transition: opacity .2s;
+  }
+
+  .slider:hover {
+    opacity: 1;
+  }
+
+  .slider::-webkit-slider-thumb {
+    -webkit-appearance: none;
+    appearance: none;
+    width: 25px;
+    height: 30px;
+    background: #f76027;
+    cursor: pointer;
+  }
+  span{
+    margin: 20px;
+  }
+  label{
+    min-width: 200px;
+  }
   </style>
