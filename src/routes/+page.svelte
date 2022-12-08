8c8f3aed3dc9ed136f52f48788e9407af79fe3f3
parckatzo <sabadogigante1717@gmail.com>
2022-12-07 22:28:15 -0300

    a
    caco

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+import Password from "./Password.svelte";
+</script>
+<div>
+  <Password />
+</div>
+
+<style src="./global.css">
+</style>
---
+++ src/routes/Password.svelte
@@ -0,0 +1,36 @@
+<main>
+    <div>
+      <h1>Password Generator</h1>
+      <h2>{password}</h2>
+      <button on:click={makeRandomPw} id="code">Generate password</button>
+      <div class="input-group">
+        <label>Password length (4-20)</label>
+        <input type="range" id="length" min='4' max='20' step="1" bind:value={passwordLength} />
+        <span>{passwordLength}</span>
+      </div>
+    </div>
+  </main>
+  
+  <script>
+  
+    let words = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let passwordLength = 8;
+    let password = "Select Password length";
+      
+    const makeRandomPw = () =>{
+      let result = ""
+      for(let i = 0; i< passwordLength; i++){
+        result += words.charAt(Math.floor(Math.random() * words.length))
+      } 
+      password = result
+    }
+  
+  </script>
+  
+  <style>
+      *{
+          padding: 0;
+          margin: 0;
+          box-sizing: border-box;
+      }
+  </style>
